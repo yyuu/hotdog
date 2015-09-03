b9dd59eabf1968215d969bd7d144ad331b1c515a
Yamashita, Yuu <peek824545201@gmail.com>
2015-09-03 13:39:15 +0900

    allow tags by tag value glob

--- lib/hotdog/commands/tags.rb
+++ lib/hotdog/commands/tags.rb
@@ -7,14 +7,31 @@ module Hotdog
         args = optparse.parse(args)
         if 0 < args.length
           fields = args.map { |tag|
-            tag_name, tag_value = tag.split(":", 2)
+            tag_name, tag_value = split_tag(tag)
             tag_name
           }
-          result1 = fields.map { |tag_name|
-            if not glob?(tag_name)
-              execute("SELECT DISTINCT value FROM tags WHERE name = ?", [tag_name]).map { |row| row.join(",") }
+          result1 = args.map { |tag|
+            tag_name, tag_value = split_tag(tag)
+            if glob?(tag_name)
+              if tag_value.empty?
+                execute("SELECT DISTINCT value FROM tags WHERE name GLOB ?", [tag_name]).map { |row| row.join(",") }
+              else
+                if glob?(tag_value)
+                  execute("SELECT DISTINCT value FROM tags WHERE name GLOB ? AND value GLOB ?", [tag_name, tag_value]).map { |row| row.join(",") }
+                else
+                  execute("SELECT DISTINCT value FROM tags WHERE name GLOB ? AND value = ?", [tag_name, tag_value]).map { |row| row.join(",") }
+                end
+              end
             else
-              execute("SELECT DISTINCT value FROM tags WHERE name GLOB ?", [tag_name]).map { |row| row.join(",") }
+              if tag_value.empty?
+                execute("SELECT DISTINCT value FROM tags WHERE name = ?", [tag_name]).map { |row| row.join(",") }
+              else
+                if glob?(tag_value)
+                  execute("SELECT DISTINCT value FROM tags WHERE name = ? AND value GLOB ?", [tag_name, tag_value]).map { |row| row.join(",") }
+                else
+                  execute("SELECT DISTINCT value FROM tags WHERE name = ? AND value = ?", [tag_name, tag_value]).map { |row| row.join(",") }
+                end
+              end
             end
           }
           result = (0...result1.reduce(0) { |max, values| [max, values.length].max }).map { |field_index|
