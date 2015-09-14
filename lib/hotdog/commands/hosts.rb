99b7700072e26d77a5912e6329c5b324e24fc05e
Yamashita, Yuu <peek824545201@gmail.com>
2015-09-14 11:31:32 +0900

    exits as error if result is insufficient

--- lib/hotdog/commands/hosts.rb
+++ lib/hotdog/commands/hosts.rb
@@ -7,23 +7,34 @@ module Hotdog
         args = optparse.parse(args)
         if args.empty?
           result = execute("SELECT id FROM hosts").to_a.reduce(:+)
+          show_hosts(result)
         else
-          result = args.flat_map { |host_name|
-            if glob?(host_name)
-              execute("SELECT id FROM hosts WHERE name GLOB ?", [host_name]).to_a.reduce(:+)
-            else
-              execute("SELECT id FROM hosts WHERE name = ?", [host_name]).to_a.reduce(:+)
+          if args.any? { |host_name| glob?(host_name) }
+            result = args.flat_map { |host_name|
+              execute("SELECT id FROM hosts WHERE name GLOB ?;", [host_name]).to_a.reduce(:+) || []
+            }
+          else
+            result = args.each_slice(SQLITE_LIMIT_COMPOUND_SELECT).flat_map { |args|
+              execute("SELECT id FROM hosts WHERE name IN (%s);" % args.map { "?" }.join(", "), args).to_a.reduce(:+) || []
+            }
+          end
+          if result.empty?
+            STDERR.puts("no match found: #{args.join(" ")}")
+            exit(1)
+          else
+            show_hosts(result)
+            logger.info("found %d host(s)." % result.length)
+            if result.length < args.length
+              STDERR.puts("insufficient result: #{args.join(" ")}")
+              exit(1)
             end
-          }
+          end
         end
-        if result && (0 < result.length)
-          result, fields = get_hosts(result)
-          STDOUT.print(format(result, fields: fields))
-          logger.info("found %d host(s)." % result.length)
-        else
-          STDERR.puts("no match found: #{args.join(" ")}")
-          exit(1)
-        end
+      end
+
+      def show_hosts(hosts)
+        result, fields = get_hosts(hosts || [])
+        STDOUT.print(format(result, fields: fields))
       end
     end
   end
