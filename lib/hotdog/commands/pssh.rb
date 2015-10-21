3961ae834e1a1e977739049aa70b89442f2784a2
Yamashita, Yuu <peek824545201@gmail.com>
2015-10-21 06:59:23 +0000

    merge common functionality of ssh and pssh

--- lib/hotdog/commands/pssh.rb
+++ lib/hotdog/commands/pssh.rb
@@ -2,130 +2,64 @@
 
 require "json"
 require "parslet"
-require "hotdog/commands/search"
+require "hotdog/commands/ssh"
 require "shellwords"
 require "parallel"
 
 module Hotdog
   module Commands
-    class Pssh < Search
+    class Pssh < SshAlike
       def define_options(optparse, options={})
-        options[:options] = []
-        options[:user] = nil
-        options[:port] = nil
-        options[:identity_file] = nil
-        options[:forward_agent] = false
+        super
         options[:max_parallelism] = nil
         options[:color] = :auto
-        options[:filter_command] = nil
 
-        optparse.on("-o SSH_OPTION", "Passes this string to ssh command through shell. This option may be given multiple times") do |option|
-          options[:options] += [option]
-        end
-        optparse.on("-i SSH_IDENTITY_FILE", "SSH identity file path") do |path|
-          options[:identity_file] = path
-        end
-        optparse.on("-A", "Enable agent forwarding", TrueClass) do |b|
-          options[:forward_agent] = b
-        end
-        optparse.on("-p PORT", "Port of the remote host", Integer) do |port|
-          options[:port] = port
-        end
-        optparse.on("-u SSH_USER", "SSH login user name") do |user|
-          options[:user] = user
-        end
         optparse.on("-P PARALLELISM", "Max parallelism", Integer) do |n|
           options[:max_parallelism] = n
-        end
-        optparse.on("-v", "--verbose", "Enable verbose ode") do |v|
-          options[:verbose] = v
         end
         optparse.on("--color=WHEN", "--colour=WHEN", "Enable colors") do |color|
           options[:color] = color
         end
-        optparse.on("--filter=COMMAND", "Command to filter search result.") do |command|
-          options[:filter_command] = command
-        end
-      end
-
-      def run(args=[], options={})
-        expression = args.join(" ").strip
-        if expression.empty?
-          # return everything if given expression is empty
-          expression = "*"
-        end
-
-        begin
-          node = parse(expression)
-        rescue Parslet::ParseFailed => error
-          STDERR.puts("syntax error: " + error.cause.ascii_tree)
-          exit(1)
-        end
-
-        result0 = evaluate(node, self)
-        if 0 < result0.length
-          exec_command(result0, options)
-        else
-          STDERR.puts("no match found: #{expression}")
-          exit(1)
-        end
       end
 
       private
-      def exec_command(result0, options={})
-        result, fields = get_hosts(result0)
-        hosts = result.flatten
-        threads = options[:max_parallelism] || hosts.size
-        use_color_p = use_color?
+      def parallelism(hosts)
+        options[:max_parallelism] || hosts.size
+      end
 
+      def filter_hosts(hosts)
         if options[:filter_command]
-          filtered_hosts = Parallel.map(hosts, in_threads: threads) { |host|
-            cmdline = build_command_string(host, options)
-            cmdline += " -- #{Shellwords.shellescape(options[:filter_command])}"
+          use_color_p = use_color?
+          filtered_hosts = Parallel.map(hosts, in_threads: parallelism(hosts)) { |host|
+            cmdline = build_command_string(host, options[:filter_command], options)
             [host, exec_command_real(host, cmdline, false, use_color_p)]
           }.select { |host, stat|
             stat
           }.map { |host, stat|
             host
           }
-          if hosts != filtered_hosts
+          if hosts == filtered_hosts
+            hosts
+          else
             logger.info("filtered host(s): #{(hosts - filtered_hosts).inspect}")
-            hosts = filtered_hosts
+            filtered_hosts
           end
-        end
-
-        stats = Parallel.map(hosts, in_threads: threads) { |host|
-          cmdline = build_command_string(host, options)
-          if @remote_command
-            cmdline += " -- #{Shellwords.shellescape(@remote_command)}"
-          end
-          exec_command_real(host, cmdline, true, use_color_p)
-        }
-        unless stats.all?
-          exit(1)
+        else
+          hosts
         end
       end
 
-      def build_command_string(host, options={})
-        # build ssh command
-        cmdline = ["ssh"]
-        if options[:forward_agent]
-          cmdline << "-A"
+      def exec_command(hosts, options={})
+        use_color_p = use_color?
+        stats = Parallel.map(hosts, in_threads: parallelism(hosts)) { |host|
+          cmdline = build_command_string(host, @remote_command, options)
+          exec_command_real(host, cmdline, true, use_color_p)
+        }
+        if stats.all?
+          exit(0)
+        else
+          exit(1)
         end
-        if options[:identity_file]
-          cmdline << "-i" << options[:identity_file]
-        end
-        if options[:user]
-          cmdline << "-l" << options[:user]
-        end
-        cmdline << "-o" << "BatchMode=yes"
-        if options[:options]
-          cmdline += options[:options].flat_map { |option| ["-o", option] }
-        end
-        if options[:port]
-          cmdline << "-p" << options[:port].to_s
-        end
-        Shellwords.join(cmdline + [host])
       end
 
       def use_color?
--- lib/hotdog/commands/ssh.rb
+++ lib/hotdog/commands/ssh.rb
@@ -7,24 +7,14 @@ require "shellwords"
 
 module Hotdog
   module Commands
-    class Ssh < Search
+    class SshAlike < Search
       def define_options(optparse, options={})
-        options[:index] = nil
         options[:options] = []
         options[:user] = nil
         options[:port] = nil
         options[:identity_file] = nil
         options[:forward_agent] = false
 
-        optparse.on("-D BIND_ADDRESS", "Specifies a local \"dynamic\" application-level port forwarding") do |bind_address|
-          options[:dynamic_port_forward] = bind_address
-        end
-        optparse.on("-L BIND_ADDRESS", "Specifies that the given port on the local (client) host is to be forwarded to the given host and port on the remote side") do |bind_address|
-          options[:port_forward] = bind_address
-        end
-        optparse.on("-n", "--index INDEX", "Use this index of host if multiple servers are found", Integer) do |index|
-          options[:index] = index
-        end
         optparse.on("-o SSH_OPTION", "Passes this string to ssh command through shell. This option may be given multiple times") do |option|
           options[:options] += [option]
         end
@@ -43,6 +33,9 @@ module Hotdog
         optparse.on("-v", "--verbose", "Enable verbose ode") do |v|
           options[:verbose] = v
         end
+        optparse.on("--filter=COMMAND", "Command to filter search result.") do |command|
+          options[:filter_command] = command
+        end
       end
 
       def run(args=[], options={})
@@ -60,54 +53,32 @@ module Hotdog
         end
 
         result0 = evaluate(node, self)
-        if 0 < result0.length
-          if result0.length == 1
-            exec_command([result0.first], options)
-          else
-            if options[:index] and options[:index] < result0.length
-              exec_command([result0[options[:index]]], options)
-            else
-              result, fields = get_hosts_with_search_tags(result0, node)
-
-              # add "index" field
-              result = result.each_with_index.map { |host, i| [i] + host }
-              fields = ["index"] + fields
-
-              STDERR.print(format(result, fields: fields))
-              logger.info("found %d host(s)." % result.length)
-              exit(1)
-            end
-          end
-        else
-          STDERR.puts("no match found: #{expression}")
-          exit(1)
-        end
-        exit(127)
+        result, fields = get_hosts_with_search_tags(result0, node)
+        hosts = filter_hosts(result.flatten)
+        validate_hosts!(hosts)
+        exec_command(hosts, options)
       end
 
       private
-      def exec_command(result0, options={})
-        result, fields = get_hosts(result0)
-        hosts = result.flatten
-        cmdline = build_command_string(hosts.first, options)
-        if @remote_command
-          cmdline += " -- #{Shellwords.shellescape(@remote_command)}"
-        end
-        logger.debug("execute: #{cmdline}")
-        exec(cmdline)
+      def filter_hosts(hosts)
+        hosts
       end
 
-      def build_command_string(host, options={})
-        # build ssh command
-        cmdline = ["ssh"]
+      def validate_hosts!(hosts)
+        if hosts.length < 1
+          STDERR.puts("no match found: #{expression}")
+          exit(1)
+        end
+      end
+
+      def exec_command(hosts, options={})
+        raise(NotImplementedError)
+      end
+
+      def build_command_options(options={})
+        cmdline = []
         if options[:forward_agent]
           cmdline << "-A"
-        end
-        if options[:dynamic_port_forward]
-          cmdline << "-D" << options[:dynamic_port_forward]
-        end
-        if options[:port_forward]
-          cmdline << "-L" << options[:port_forward]
         end
         if options[:identity_file]
           cmdline << "-i" << options[:identity_file]
@@ -124,7 +95,69 @@ module Hotdog
         if options[:verbose]
           cmdline << "-v"
         end
-        Shellwords.join(cmdline + [host])
+        cmdline
+      end
+
+      def build_command_string(host, command=nil, options={})
+        # build ssh command
+        cmdline = ["ssh"] + build_command_options(options) + [host]
+        if command
+          cmdline << "--" << command
+        end
+        Shellwords.join(cmdline)
+      end
+    end
+
+    class Ssh < SshAlike
+      def define_options(optparse, options={})
+        super
+        options[:index] = nil
+        optparse.on("-D BIND_ADDRESS", "Specifies a local \"dynamic\" application-level port forwarding") do |bind_address|
+          options[:dynamic_port_forward] = bind_address
+        end
+        optparse.on("-L BIND_ADDRESS", "Specifies that the given port on the local (client) host is to be forwarded to the given host and port on the remote side") do |bind_address|
+          options[:port_forward] = bind_address
+        end
+        optparse.on("-n", "--index INDEX", "Use this index of host if multiple servers are found", Integer) do |index|
+          options[:index] = index
+        end
+      end
+
+      private
+      def filter_hosts(hosts)
+        if options[:index] and options[:index] < hosts.length
+          [hosts[options[:index]]]
+        else
+          hosts
+        end
+      end
+
+      def validate_hosts!(hosts)
+        super
+        if hosts.length != 1
+          result = hosts.each_with_index.map { |host, i| [i, host] }
+          STDERR.print(format(result, fields: ["index", "host"]))
+          logger.error("found %d hosts." % result.length)
+          exit(1)
+        end
+      end
+
+      def exec_command(hosts, options={})
+        cmdline = build_command_string(hosts.first, @remote_command, options)
+        logger.debug("execute: #{cmdline}")
+        exec(cmdline)
+        exit(127)
+      end
+
+      def build_command_options(options={})
+        arguments = super
+        if options[:dynamic_port_forward]
+          arguments << "-D" << options[:dynamic_port_forward]
+        end
+        if options[:port_forward]
+          arguments << "-L" << options[:port_forward]
+        end
+        arguments
       end
     end
   end
