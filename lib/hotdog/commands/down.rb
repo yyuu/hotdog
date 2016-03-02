a4f3271c558ecaa8328be395c5745ccb8a05c6fd
Yamashita, Yuu <peek824545201@gmail.com>
2016-03-02 08:31:15 +0000

    Retry after API call failure on up/down

--- lib/hotdog/commands/down.rb
+++ lib/hotdog/commands/down.rb
@@ -8,8 +8,15 @@ module Hotdog
       def define_options(optparse, options={})
         options[:downtime] = 86400
         options[:start] = Time.new
+        options[:retry] = 5
         optparse.on("--downtime DURATION") do |v|
           options[:downtime] = v.to_i
+        end
+        optparse.on("--retry NUM") do |v|
+          options[:retry] = v.to_i
+        end
+        optparse.on("--retry-delay SECONDS") do |v|
+          options[:retry_delay] = v.to_i
         end
         optparse.on("--start TIME") do |v|
           options[:start] = Time.parse(v)
@@ -23,10 +30,18 @@ module Hotdog
           else
             scope = arg
           end
-          code, schedule = dog.schedule_downtime(scope, :start => options[:start].to_i, :end => (options[:start]+options[:downtime]).to_i)
-          logger.debug("dog.schedule_donwtime(%s, :start => %s, :end => %s) #==> [%s, %s]" % [scope.inspect, options[:start].to_i, (options[:start]+options[:downtime]).to_i, code.inspect, schedule.inspect])
-          if code.to_i / 100 != 2
-            raise("dog.schedule_downtime(%s, ...) returns [%s, %s]" % [scope.inspect, code.inspect, schedule.inspect])
+          if 0 < options[:retry]
+            options[:retry].times do |i|
+              begin
+                schedule_downtime(scope, options)
+                break
+              rescue => error
+                logger.warn(error.to_s)
+                sleep(options[:retry_delay] || (1<<i))
+              end
+            end
+          else
+            schedule_downtime(scope, options)
           end
         end
 
@@ -35,6 +50,16 @@ module Hotdog
           close_db(@db)
         end
         FileUtils.rm_f(File.join(options[:confdir], PERSISTENT_DB))
+      end
+
+      private
+      def schedule_downtime(scope, options={})
+        code, schedule = dog.schedule_downtime(scope, :start => options[:start].to_i, :end => (options[:start]+options[:downtime]).to_i)
+        logger.debug("dog.schedule_donwtime(%s, :start => %s, :end => %s) #==> [%s, %s]" % [scope.inspect, options[:start].to_i, (options[:start]+options[:downtime]).to_i, code.inspect, schedule.inspect])
+        if code.to_i / 100 != 2
+          raise("dog.schedule_downtime(%s, ...) returns [%s, %s]" % [scope.inspect, code.inspect, schedule.inspect])
+        end
+        schedule
       end
     end
   end
--- lib/hotdog/commands/up.rb
+++ lib/hotdog/commands/up.rb
@@ -5,6 +5,16 @@ require "fileutils"
 module Hotdog
   module Commands
     class Up < BaseCommand
+      def define_options(optparse, options={})
+        options[:retry] = 5
+        optparse.on("--retry NUM") do |v|
+          options[:retry] = v.to_i
+        end
+        optparse.on("--retry-delay SECONDS") do |v|
+          options[:retry_delay] = v.to_i
+        end
+      end
+
       def run(args=[], options={})
         scopes = args.map { |arg|
           if arg.index(":").nil?
@@ -13,9 +23,19 @@ module Hotdog
             arg
           end
         }
-        code, all_downtimes = dog.get_all_downtimes()
-        if code.to_i / 100 != 2
-          raise("dog.get_all_downtimes() returns [%s, %s]" % [code.inspect, all_downtimes.inspect])
+        all_downtimes = nil
+        if 0 < options[:retry]
+          options[:retry].times do |i|
+            begin
+              all_downtimes = get_all_downtimes(options)
+              break
+            rescue => error
+              logger.warn(error.to_s)
+              sleep(options[:retry_delay] || (1<<i))
+            end
+          end
+        else
+          all_downtimes = get_all_downtimes(options)
         end
 
         cancel_downtimes = all_downtimes.select { |downtime|
@@ -23,9 +43,18 @@ module Hotdog
         }
 
         cancel_downtimes.each do |downtime|
-          code, cancel = dog.cancel_downtime(downtime["id"])
-          if code.to_i / 100 != 2
-            raise("dog.cancel_downtime(%s) returns [%s, %s]" % [downtime["id"].inspect, code.inspect, cancel.inspect])
+          if 0 < options[:retry]
+            options[:retry].times do |i|
+              begin
+                cancel_downtime(downtime["id"], options)
+                break
+              rescue => error
+                logger.warn(error.to_s)
+                sleep(options[:retry_delay] || (1<<i))
+              end
+            end
+          else
+            cancel_downtime(downtime["id"], options)
           end
         end
 
@@ -34,6 +63,23 @@ module Hotdog
           close_db(@db)
         end
         FileUtils.rm_f(File.join(options[:confdir], PERSISTENT_DB))
+      end
+
+      private
+      def get_all_downtimes(options={})
+        code, all_downtimes = dog.get_all_downtimes()
+        if code.to_i / 100 != 2
+          raise("dog.get_all_downtimes() returns [%s, %s]" % [code.inspect, all_downtimes.inspect])
+        end
+        all_downtimes
+      end
+
+      def cancel_downtime(id, options={})
+        code, cancel = dog.cancel_downtime(id)
+        if code.to_i / 100 != 2
+          raise("dog.cancel_downtime(%s) returns [%s, %s]" % [id.inspect, code.inspect, cancel.inspect])
+        end
+        cancel
       end
     end
   end
