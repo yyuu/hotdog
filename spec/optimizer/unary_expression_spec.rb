037febacd56f97b7e6a9ad0e1abf5d8417bcf4a4
Yamashita, Yuu <peek824545201@gmail.com>
2017-08-28 14:34:35 +0000

    Run optimize for N times

--- spec/optimizer/unary_expression_spec.rb
+++ spec/optimizer/unary_expression_spec.rb
@@ -5,323 +5,325 @@ require "hotdog/commands/search"
 require "parslet"
 
 describe "unary expression" do
-  it "NOT nothing should return everything" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new("NOT", Hotdog::Expression::NothingNode.new())
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts;",
-      values: [],
-    })
-  end
+  3.times do |o|
+    it "NOT nothing should return everything (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new("NOT", Hotdog::Expression::NothingNode.new())
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts;",
+        values: [],
+      })
+    end
 
-  it "NOT everything should return nothing" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new("NOT", Hotdog::Expression::EverythingNode.new())
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT NULL AS host_id WHERE host_id NOT NULL;",
-      values: [],
-    })
-  end
+    it "NOT everything should return nothing (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new("NOT", Hotdog::Expression::EverythingNode.new())
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT NULL AS host_id WHERE host_id NOT NULL;",
+        values: [],
+      })
+    end
 
-  it "NOT NOT nothing should return nothing" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
-        "NOT",
-        Hotdog::Expression::NothingNode.new(),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT NULL AS host_id WHERE host_id NOT NULL;",
-      values: [],
-    })
-  end
-
-  it "NOT NOT everything should return everything" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
-        "NOT",
-        Hotdog::Expression::EverythingNode.new(),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts;",
-      values: [],
-    })
-  end
-
-  it "NOT NOT NOT nothing should return everything" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOT NOT nothing should return nothing (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOT",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOT",
           Hotdog::Expression::NothingNode.new(),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts;",
-      values: [],
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT NULL AS host_id WHERE host_id NOT NULL;",
+        values: [],
+      })
+    end
 
-  it "NOT NOT NOT everything should return nothing" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOT NOT everything should return everything (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOT",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOT",
           Hotdog::Expression::EverythingNode.new(),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT NULL AS host_id WHERE host_id NOT NULL;",
-      values: [],
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts;",
+        values: [],
+      })
+    end
 
-  it "NOT host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::StringHostNode.new("foo", ":"),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
-      values: ["foo"],
-    })
-  end
+    it "NOT NOT NOT nothing should return everything (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
+        "NOT",
+        Hotdog::Expression::UnaryExpressionNode.new(
+          "NOT",
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOT",
+            Hotdog::Expression::NothingNode.new(),
+          ),
+        ),
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts;",
+        values: [],
+      })
+    end
 
-  it "NOT NOT host should return the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOT NOT NOT everything should return nothing (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
+        "NOT",
+        Hotdog::Expression::UnaryExpressionNode.new(
+          "NOT",
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOT",
+            Hotdog::Expression::EverythingNode.new(),
+          ),
+        ),
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT NULL AS host_id WHERE host_id NOT NULL;",
+        values: [],
+      })
+    end
+
+    it "NOT host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOT",
         Hotdog::Expression::StringHostNode.new("foo", ":"),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
+        values: ["foo"],
+      })
+    end
 
-  it "NOT NOT NOT host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOT NOT host should return the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOT",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOT",
           Hotdog::Expression::StringHostNode.new("foo", ":"),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
-      values: ["foo"],
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
 
-  it "NOOP host should return the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOOP",
-      Hotdog::Expression::StringHostNode.new("foo", ":"),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
-  end
+    it "NOT NOT NOT host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
+        "NOT",
+        Hotdog::Expression::UnaryExpressionNode.new(
+          "NOT",
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOT",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
+        ),
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
+        values: ["foo"],
+      })
+    end
 
-  it "NOOP NOOP host should return the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOOP",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOOP host should return the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOOP",
         Hotdog::Expression::StringHostNode.new("foo", ":"),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
 
-  it "NOOP NOOP NOOP host should return the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOOP",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOOP NOOP host should return the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOOP",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOOP",
           Hotdog::Expression::StringHostNode.new("foo", ":"),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
 
-  it "NOT NOOP NOOP host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOOP NOOP NOOP host should return the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOOP",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOOP",
-          Hotdog::Expression::StringHostNode.new("foo", ":"),
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOOP",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
-      values: ["foo"],
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
 
-  it "NOOP NOT NOOP host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOOP",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOT NOOP NOOP host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOT",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOOP",
-          Hotdog::Expression::StringHostNode.new("foo", ":"),
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOOP",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
-      values: ["foo"],
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
+        values: ["foo"],
+      })
+    end
 
-  it "NOOP NOOP NOT host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOOP",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOOP NOT NOOP host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOOP",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOT",
-          Hotdog::Expression::StringHostNode.new("foo", ":"),
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOOP",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
-      values: ["foo"],
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
+        values: ["foo"],
+      })
+    end
 
-  it "NOOP NOT NOT host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
-        "NOT",
-        Hotdog::Expression::UnaryExpressionNode.new(
-          "NOOP",
-          Hotdog::Expression::StringHostNode.new("foo", ":"),
-        ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
-  end
-
-  it "NOT NOOP NOT host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOOP NOOP NOT host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOOP",
         Hotdog::Expression::UnaryExpressionNode.new(
-          "NOT",
-          Hotdog::Expression::StringHostNode.new("foo", ":"),
+          "NOOP",
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOT",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
-  end
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        query: "SELECT id AS host_id FROM hosts EXCEPT SELECT hosts.id AS host_id FROM hosts WHERE hosts.name = ?;",
+        values: ["foo"],
+      })
+    end
 
-  it "NOT NOT NOOP host should return everything except the host" do
-    expr = Hotdog::Expression::UnaryExpressionNode.new(
-      "NOT",
-      Hotdog::Expression::UnaryExpressionNode.new(
+    it "NOOP NOT NOT host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
+        "NOT",
+        Hotdog::Expression::UnaryExpressionNode.new(
+          "NOT",
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOOP",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
+        ),
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
+
+    it "NOT NOOP NOT host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
         "NOT",
         Hotdog::Expression::UnaryExpressionNode.new(
           "NOOP",
-          Hotdog::Expression::StringHostNode.new("foo", ":"),
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOT",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
         ),
-      ),
-    )
-    expect(expr.optimize.optimize.optimize.dump).to eq({
-      tagname: "host",
-      separator: ":",
-      tagvalue: "foo",
-      fallback: {
-        query: [
-          "SELECT hosts.id AS host_id FROM hosts",
-          "WHERE LOWER(hosts.name) GLOB LOWER(?);",
-        ].join(" "),
-        values: ["*foo*"],
-      },
-    })
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
+
+    it "NOT NOT NOOP host should return everything except the host (#{o})" do
+      expr = Hotdog::Expression::UnaryExpressionNode.new(
+        "NOT",
+        Hotdog::Expression::UnaryExpressionNode.new(
+          "NOT",
+          Hotdog::Expression::UnaryExpressionNode.new(
+            "NOOP",
+            Hotdog::Expression::StringHostNode.new("foo", ":"),
+          ),
+        ),
+      )
+      expect(optimize_n(o+1, expr).dump).to eq({
+        tagname: "host",
+        separator: ":",
+        tagvalue: "foo",
+        fallback: {
+          query: [
+            "SELECT hosts.id AS host_id FROM hosts",
+            "WHERE LOWER(hosts.name) GLOB LOWER(?);",
+          ].join(" "),
+          values: ["*foo*"],
+        },
+      })
+    end
   end
 end
