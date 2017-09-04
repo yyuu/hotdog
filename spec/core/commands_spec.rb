daa313a7373326782317d16e83fc6ebf580f2c0c
Yamashita, Yuu <peek824545201@gmail.com>
2017-09-04 05:07:34 +0000

    Fix corrupted tests

--- spec/core/commands_spec.rb
+++ spec/core/commands_spec.rb
@@ -4,6 +4,8 @@ require "hotdog/commands"
 require "hotdog/commands/search"
 
 describe "commands" do
+  let(:default_host_mode) { 0 }
+
   let(:cmd) {
     Hotdog::Commands::Search.new(Hotdog::Application.new)
   }
@@ -50,10 +52,11 @@ describe "commands" do
     allow(cmd).to receive(:update_db)
     q1 = [
       "SELECT DISTINCT tags.name FROM hosts_tags",
+        "INNER JOIN hosts ON hosts_tags.host_id = hosts.id",
         "INNER JOIN tags ON hosts_tags.tag_id = tags.id",
-          "WHERE hosts_tags.host_id IN (?, ?, ?) ORDER BY hosts_tags.host_id;",
+          "WHERE hosts.mode = ? AND hosts.id IN (?, ?, ?) ORDER BY hosts.id;",
     ]
-    allow(cmd).to receive(:execute).with(q1.join(" "), [1, 2, 3]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 1, 2, 3]) {
       [["foo"], ["bar"], ["baz"]]
     }
     allow(cmd).to receive(:get_hosts_fields).with([1, 2, 3], ["host", "foo", "bar", "baz"])
@@ -66,10 +69,11 @@ describe "commands" do
     allow(cmd).to receive(:update_db)
     q1 = [
       "SELECT DISTINCT tags.name FROM hosts_tags",
+        "INNER JOIN hosts ON hosts_tags.host_id = hosts.id",
         "INNER JOIN tags ON hosts_tags.tag_id = tags.id",
-          "WHERE hosts_tags.host_id IN (?, ?, ?) ORDER BY hosts_tags.host_id;",
+          "WHERE hosts.mode = ? AND hosts.id IN (?, ?, ?) ORDER BY hosts.id;",
     ]
-    allow(cmd).to receive(:execute).with(q1.join(" "), [1, 2, 3]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 1, 2, 3]) {
       [["foo"], ["bar"], ["baz"]]
     }
     allow(cmd).to receive(:get_hosts_fields).with([1, 2, 3], ["bar", "host", "foo", "baz"])
@@ -83,17 +87,18 @@ describe "commands" do
   it "get host fields without host" do
     q1 = [
       "SELECT LOWER(tags.name), GROUP_CONCAT(tags.value, ',') FROM hosts_tags",
+        "INNER JOIN hosts ON hosts_tags.host_id = hosts.id",
         "INNER JOIN tags ON hosts_tags.tag_id = tags.id",
-          "WHERE hosts_tags.host_id = ? AND tags.name IN (?, ?, ?)",
+          "WHERE hosts.mode = ? AND hosts.id = ? AND tags.name IN (?, ?, ?)",
             "GROUP BY tags.name;",
     ]
-    allow(cmd).to receive(:execute).with(q1.join(" "), [1, "foo", "bar", "baz"]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 1, "foo", "bar", "baz"]) {
       [["foo", "foo1"], ["bar", "bar1"], ["baz", "baz1"]]
     }
-    allow(cmd).to receive(:execute).with(q1.join(" "), [2, "foo", "bar", "baz"]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 2, "foo", "bar", "baz"]) {
       [["foo", "foo2"], ["bar", "bar2"], ["baz", "baz2"]]
     }
-    allow(cmd).to receive(:execute).with(q1.join(" "), [3, "foo", "bar", "baz"]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 3, "foo", "bar", "baz"]) {
       [["foo", "foo3"], ["bar", "bar3"], ["baz", "baz3"]]
     }
     expect(cmd.__send__(:get_hosts_fields, [1, 2, 3], ["foo", "bar", "baz"])).to eq([[["foo1", "bar1", "baz1"], ["foo2", "bar2", "baz2"], ["foo3", "bar3", "baz3"]], ["foo", "bar", "baz"]])
@@ -102,17 +107,18 @@ describe "commands" do
   it "get host fields with host" do
     q1 = [
       "SELECT LOWER(tags.name), GROUP_CONCAT(tags.value, ',') FROM hosts_tags",
+        "INNER JOIN hosts ON hosts_tags.host_id = hosts.id",
         "INNER JOIN tags ON hosts_tags.tag_id = tags.id",
-          "WHERE hosts_tags.host_id = ? AND tags.name IN (?, ?, ?)",
+          "WHERE hosts.mode = ? AND hosts.id = ? AND tags.name IN (?, ?, ?)",
             "GROUP BY tags.name;",
     ]
-    allow(cmd).to receive(:execute).with(q1.join(" "), [1, "foo", "bar", "host"]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 1, "foo", "bar", "host"]) {
       [["foo", "foo1"], ["bar", "bar1"], ["host", "host1"]]
     }
-    allow(cmd).to receive(:execute).with(q1.join(" "), [2, "foo", "bar", "host"]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 2, "foo", "bar", "host"]) {
       [["foo", "foo2"], ["bar", "bar2"], ["host", "host2"]]
     }
-    allow(cmd).to receive(:execute).with(q1.join(" "), [3, "foo", "bar", "host"]) {
+    allow(cmd).to receive(:execute).with(q1.join(" "), [default_host_mode, 3, "foo", "bar", "host"]) {
       [["foo", "foo3"], ["bar", "bar3"], ["host", "host3"]]
     }
     expect(cmd.__send__(:get_hosts_fields, [1, 2, 3], ["foo", "bar", "host"])).to eq([[["foo1", "bar1", "host1"], ["foo2", "bar2", "host2"], ["foo3", "bar3", "host3"]], ["foo", "bar", "host"]])
